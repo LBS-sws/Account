58f3c56fe3f86b2273b9c34ea637e1a9b4d6a3b1
LBS IT <percylee@lbsgroup.com.hk>
2019-11-27 10:15:00 +0800

    Change Wording

--- acct/protected/messages/zh_cn/block.php
+++ acct/protected/messages/zh_cn/block.php
@@ -1,5 +1,5 @@
 <?php
 return array(
-	'Please complete Personnel System - Appraisial before using other functions.'=>'在使用其他功能之前，请先完成人事系统-评估。'
+	'Please complete Personnel System - Appraisial before using other functions.'=>'在使用其他功能前，请先完成人事系统-优化人才评核。'
 );
 ?>
--- acct/protected/messages/zh_tw/block.php
+++ acct/protected/messages/zh_tw/block.php
@@ -1,5 +1,5 @@
 <?php
 return array(
-	'Please complete Personnel System - Appraisial before using other functions.'=>'在使用其他功能之前，請先完成人事系統-評估。'
+	'Please complete Personnel System - Appraisial before using other functions.'=>'在使用其他功能前，請先完成人事系統-優化人才評核。'
 );
 ?>
