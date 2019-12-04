df9de6f9e9173e2167fb1ea68771a6f5b7c8ab46
LBS IT <percylee@lbsgroup.com.hk>
2019-12-04 15:12:55 +0800

    限制功能修改

--- acct/protected/messages/zh_cn/block.php
+++ acct/protected/messages/zh_cn/block.php
@@ -1,5 +1,9 @@
-﻿<?php
+<?php
 return array(
-    'Please complete Personnel System - Appraisial before using other functions.'=>'在使用其他功能前，请先完成人事系统-优化人才评核。'
+	'Please complete Personnel System - Appraisial before using other functions.'=>'在使用其他功能前，请先完成人事系统-优化人才评核。',
+	'Please complete Operation System - Sales Summary Report Approval before using other functions.'
+	=>'在使用其他功能前，请先完成营运系统-营业报告审核。',
+	'Please complete Operation System - Sales Summary Report Submission before using other functions.'
+	=>'在使用其他功能前，请先完成营运系统-营业报告提交。',
 );
 ?>
