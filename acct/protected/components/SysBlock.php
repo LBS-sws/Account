f07282982ad59a6f33c1c78774677c150da2277d
LBS IT <percylee@lbsgroup.com.hk>
2020-03-04 18:11:21 +0800

    同步

--- acct/protected/components/SysBlock.php
+++ acct/protected/components/SysBlock.php
@@ -9,7 +9,6 @@ class SysBlock {
 	public function blockNRoute($controllerId, $functionId) {
 		$session = Yii::app()->session;
 		$sysblock =isset($session['sysblock']) ? $session['sysblock'] : array();
-		
 		$sysId = Yii::app()->params['systemId'];
 		
 		foreach ($this->checkItems as $key=>$value) {
@@ -62,15 +61,16 @@ class SysBlock {
 			$year = date("Y");
 			$day = date("m-d");
 			if($day>="11-01"){
-				$dateSql = " and ((b.year<=".($year-1).") or (b.year = $year and year_type = 1))";
+				$dateSql = " and ((b.year<=".($year-1).") or (b.year = $year and b.year_type = 1))";
 			}elseif ($day>="05-01"){
-			$dateSql = " and b.year<=".($year-1);
+			    $dateSql = " and b.year<=".($year-1);
 			}else{
-				$dateSql = " and ((b.year<=".($year-2).") or (b.year = ".($year-1)." and year_type = 1))";
+				$dateSql = " and ((b.year<=".($year-2).") or (b.year = ".($year-1)." and b.year_type = 1))";
 			}
 			$count = Yii::app()->db->createCommand()->select("a.id")->from("hr$suffix.hr_review_h a")
 				->leftJoin("hr$suffix.hr_review b","a.review_id=b.id")
-				->where("a.status_type!=3 and a.handle_id=:handle_id $dateSql",
+				->leftJoin("hr$suffix.hr_employee d","b.employee_id=d.id")
+				->where("d.staff_status=0 and a.status_type!=3 and a.handle_id=:handle_id $dateSql",
 					array(":handle_id"=>$row['id'])
 				)->queryRow();
 			if($count){ //存在未考核的員工
@@ -143,63 +143,63 @@ class SysBlock {
 		return ($row===false);
 	}
 
-    /**
-    每年12月30日, 驗證 用户有学分确认权限的未及时处理完, false: 未处理
-     **/
-    public function isCreditConfirmed() {
-        $uid = Yii::app()->user->id;
-        $city = Yii::app()->user->city();
+	/** 
+		每年12月30日, 驗證 用户有学分确认权限的未及时处理完, false: 未处理
+	**/
+	public function isCreditConfirmed() {
+		$uid = Yii::app()->user->id;
+		$city = Yii::app()->user->city();
         $city_allow = Yii::app()->user->city_allow();
-        $suffix = Yii::app()->params['envSuffix'];
-        $lastdate = date('m-d')=='12-31' ? date('Y-m-d') : date('Y-m-d',strtotime('last year December 31st'));
-        $year = date("Y", strtotime($lastdate));
-        $month = date("m", strtotime($lastdate));
+		$suffix = Yii::app()->params['envSuffix'];
+		$lastdate = date('m-d')=='12-31' ? date('Y-m-d') : date('Y-m-d',strtotime('last year December 31st'));
+		$year = date("Y", strtotime($lastdate));
+		$month = date("m", strtotime($lastdate));
 
-        $sql = "select a_control from security$suffix.sec_user_access 
+		$sql = "select a_control from security$suffix.sec_user_access 
 				where username='$uid' and system_id='sp' and a_read_write like '%GA04%'
 			";
-        $row = Yii::app()->db->createCommand($sql)->queryRow();
-        if ($row===false) return true;
+		$row = Yii::app()->db->createCommand($sql)->queryRow();
+		if ($row===false) return true;
 
-        $sql = "select a.id from spoint$suffix.gr_credit_request a
+		$sql = "select a.id from spoint$suffix.gr_credit_request a
                 LEFT JOIN spoint$suffix.gr_credit_type b ON a.credit_type = b.id
                 LEFT JOIN hr$suffix.hr_employee d ON a.employee_id = d.id
                 where d.city='$city' AND a.state = 1 and a.apply_date <= '$lastdate'
 				limit 1
 			";
-        $row = Yii::app()->db->createCommand($sql)->queryRow();
-        return ($row===false);
-    }
+		$row = Yii::app()->db->createCommand($sql)->queryRow();
+		return ($row===false);
+	}
 
-    /**
-    每年12月30日, 驗證 用户有学分审核权限的未及时处理完, false: 未处理
-     **/
-    public function isCreditApproved() {
-        $uid = Yii::app()->user->id;
-        $city = Yii::app()->user->city();
+	/** 
+		每年12月30日, 驗證 用户有学分审核权限的未及时处理完, false: 未处理
+	**/
+	public function isCreditApproved() {
+		$uid = Yii::app()->user->id;
+		$city = Yii::app()->user->city();
         $city_allow = Yii::app()->user->city_allow();
-        $suffix = Yii::app()->params['envSuffix'];
-        $lastdate = date('m-d')=='12-31' ? date('Y-m-d') : date('Y-m-d',strtotime('last year December 31st'));
-        $year = date("Y", strtotime($lastdate));
-        $month = date("m", strtotime($lastdate));
+		$suffix = Yii::app()->params['envSuffix'];
+		$lastdate = date('m-d')=='12-31' ? date('Y-m-d') : date('Y-m-d',strtotime('last year December 31st'));
+		$year = date("Y", strtotime($lastdate));
+		$month = date("m", strtotime($lastdate));
 
-        $sql = "select a_control from security$suffix.sec_user_access 
+		$sql = "select a_control from security$suffix.sec_user_access 
 				where username='$uid' and system_id='sp' and a_read_write like '%GA01%'
 			";
-        $row = Yii::app()->db->createCommand($sql)->queryRow();
-        if ($row===false) return true;
+		$row = Yii::app()->db->createCommand($sql)->queryRow();
+		if ($row===false) return true;
 
-        $sql = "select a.id from spoint$suffix.gr_credit_request a
+		$sql = "select a.id from spoint$suffix.gr_credit_request a
                 LEFT JOIN spoint$suffix.gr_credit_type b ON a.credit_type = b.id
                 LEFT JOIN hr$suffix.hr_employee d ON a.employee_id = d.id
                 where d.city='$city' AND a.state = 4 and a.apply_date <= '$lastdate'
 				limit 1
 			";
-        $row = Yii::app()->db->createCommand($sql)->queryRow();
-        return ($row===false);
-    }
+		$row = Yii::app()->db->createCommand($sql)->queryRow();
+		return ($row===false);
+	}
 
-    public function test() {
+	public function test() {
 		return false;
 	}
 }
