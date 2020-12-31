eb96280f9c38aa963d3378a198f9fd808a8a0c78
LBS IT <percylee@lbsgroup.com.hk>
2020-12-31 16:31:28 +0800

    統一信息檔

--- acct/protected/messages/zh_tw/block.php
+++ acct/protected/messages/zh_tw/block.php
@@ -5,5 +5,11 @@ return array(
 	=>'在使用其他功能前，請先完成營運系統-營業報告審核。 ',
 	'Please complete Operation System - Sales Summary Report Submission before using other functions.'
 	=>'在使用其他功能前，請先完成營運系統-營業報告提交。 ',
+	'Please complete Academic Credit System - Credit Request Approval before using other functions.'
+	=>'在使用其他功能前，請先完成學分系統-學分審核。 ',
+	'Please complete Academic Credit System - Credit Request Confirmation before using other functions.'
+	=>'在使用其他功能前，請先完成學分系統-學分專員確認。 ',
+    'validateExamination'=>'上月服務客戶質檢出的平均分低於75分，請登錄測驗系統進行測驗做題<br/>要求：答題正確率為85%, 否則需繼續測驗',
+    'validateNewStaff'=>'歡迎加入史偉莎, 為確保服務品質<br>請於到職日起算三個月內, 完成第一次的培訓測驗<br/>要求：答题正确率为85%, 否则需继续测验',
 );
 ?>
