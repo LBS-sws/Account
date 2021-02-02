9bb1053f854045f926eb2bf1f633ca8dcd776adf
anxiaxia <1286571499@qq.com>
2021-02-02 15:10:56 +0800

    提成表状态颜色显示

--- acct/protected/views/salestable/_listdtl.php
+++ acct/protected/views/salestable/_listdtl.php
@@ -1,4 +1,4 @@
-<tr class='clickable-row' data-href='<?php echo $this->getLink('XS07', 'salestable/edit', 'salestable/view', array('index'=>$this->record['id']));?>'>
+<tr  <?php if($this->record['examine']=='待审核'){echo "style='color: blue'";} if($this->record['examine']=='审核通过'){echo "style='color: green'";}?>  class='clickable-row' data-href='<?php echo $this->getLink('XS07', 'salestable/edit', 'salestable/view', array('index'=>$this->record['id']));?>'>
 
 	<td><?php echo $this->record['employee_code']; ?></td>
 	<td><?php echo $this->record['employee_name']; ?></td>
