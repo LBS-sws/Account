2193bb2414cab2147cbe07813cc0dbbb649292bc
anxiaxia <1286571499@qq.com>
2021-02-22 11:03:25 +0800

    东成西就计划取消佛山

--- acct/protected/models/ReportXS02Form.php
+++ acct/protected/models/ReportXS02Form.php
@@ -135,7 +135,7 @@ class ReportXS02Form extends CReportForm
             $date=$records['year_no']."/".$records['month_no'].'/'."01";
             $date1='2020/07/01';
             $employee=$this->getEmployee($records['employee_code'],$records['year_no'],$records['month_no']);
-            if($records['city']=='CD'||$records['city']=='FS'||$records['city']=='NJ'||$records['city']=='TJ'||$a==1||strtotime($date)<strtotime($date1)||$employee==1){
+            if($records['city']=='CD'||$records['city']=='NJ'||$records['city']=='TJ'||$a==1||strtotime($date)<strtotime($date1)||$employee==1){
                 $month=$records['month_no'];
                 $year=$records['year_no'];
             }else{
