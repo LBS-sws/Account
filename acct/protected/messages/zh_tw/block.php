b8f22ccb4a8e259935621f56ceb216a6555ccfbd
LBS IT <percylee@lbsgroup.com.hk>
2021-04-01 15:29:05 +0800

    月报表总结限制功能 - 文字

--- acct/protected/messages/zh_tw/block.php
+++ acct/protected/messages/zh_tw/block.php
@@ -11,5 +11,7 @@ return array(
 	=>'在使用其他功能前，請先完成學分系統-學分專員確認。 ',
     'validateExamination'=>'上月服務客戶質檢出的平均分低於75分，請登錄測驗系統進行測驗做題<br/>要求：答題正確率為85%, 否則需繼續測驗',
     'validateNewStaff'=>'歡迎加入史偉莎, 為確保服務品質<br>請於到職日起算三個月內, 完成第一次的培訓測驗<br/>要求：答题正确率为85%, 否则需继续测验',
+   'Please complete Report System - Sales Summary Report Submission before using other functions.'
+    =>'在使用其他功能前，請先完成日报表系统-月报表总结提交。',
 );
 ?>
