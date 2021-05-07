9af9c6f067d051832d82cbceea33f86d5dbc7c59
jixiaoyan96 <jxy.cd@lbsgroup.com.cn>
2021-05-07 10:25:48 +0800

    产品生意提成，产品提成问题04

--- acct/protected/models/ReportXS01List.php
+++ acct/protected/models/ReportXS01List.php
@@ -2212,7 +2212,7 @@ class ReportXS01List extends CListPageModel
                    left outer join swoper$suffix.swo_task  b on  b.id=a.task
             where a.id='$ai'";
             $records = Yii::app()->db->createCommand($sql)->queryRow();
-            var_dump($money);
+            var_dump($money);die();
             $fuwu=$this->getProductctAmount($city,$records['task'],$records['sales_products'],$date,$money);//本单产品提成比例
             $fuwu=$fuwu+$point['point'];
             $mons+=$records['money']*$fuwu*$records['qty'];
@@ -2236,7 +2236,7 @@ class ReportXS01List extends CListPageModel
     public  function getProductctAmount($city, $cust_type,$sales_products, $start_dt, $sales_amt) {
         //城市，类别，时间，总金额
         $rtn = 0;
-        if (!empty($city) && !empty($cust_type) && !empty($start_dt) && !empty($sales_amt)) {
+        if (!empty($city) && !empty($cust_type) && !empty($start_dt) && $sales_amt) {//!empty($sales_amt)
             $suffix = Yii::app()->params['envSuffix'];
             //客户类别
             //  $sql = "select rpt_cat from swoper$suffix.swo_customer_type where id=$cust_type";
