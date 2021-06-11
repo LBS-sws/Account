f83c3f526bfb3211461766f4730117f1f93e10d5
shenchao <shenchao@lbsgroup.com.cn>
2021-06-11 15:03:27 +0800

    銷售提成的產品計算添加清除按鈕

--- acct/protected/views/commission/product.php
+++ acct/protected/views/commission/product.php
@@ -30,9 +30,9 @@ $this->pageTitle=Yii::app()->name . ' - Month Report';
             <?php  echo TbHtml::button('<span class="fa fa-upload"></span> '.Yii::t('misc','Save1'), array(
                     'submit'=>Yii::app()->createUrl('commission/productsave',array('year'=>$year,'month'=>$month,'index'=>$index)))
             ); ?>
-<!--            --><?php //echo TbHtml::button('<span class="fa fa-upload"></span> '.Yii::t('misc','Clear'), array(
-//                    'submit'=>Yii::app()->createUrl('commission/clear',array('year'=>$year,'month'=>$month,'index'=>$index,'clear'=>'performanceend')))
-//            ); ?>
+            <?php echo TbHtml::button('<span class="fa fa-upload"></span> '.Yii::t('misc','Clear'), array(
+                    'submit'=>Yii::app()->createUrl('commission/clear',array('year'=>$year,'month'=>$month,'index'=>$index,'clear'=>'product')))
+            ); ?>
         </div>
     </div>
 </div>
