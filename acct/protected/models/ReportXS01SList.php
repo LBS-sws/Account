7b3e712427e0bc2a2a5641089f7c11de6f0d6146
LBS IT <shenchao@lbsgroup.com.cn>
2021-07-07 17:52:10 +0800

    優化報錯提示
    優化報錯提示

--- acct/protected/models/ReportXS01SList.php
+++ acct/protected/models/ReportXS01SList.php
@@ -1205,7 +1205,7 @@ class ReportXS01SList extends CListPageModel
                 $records['salesman']=str_replace('(','',$records['salesman']);
                 $records['salesman']=str_replace(')','',$records['salesman']);
                 if(empty($records['city'])){
-                    Dialog::message(Yii::t('dialog','Validation Message'),Yii::t('dialog','Data is filled in incorrectly, please check and modify before proceeding')." service_id:".$records["id"] );
+                    Dialog::message(Yii::t('dialog','Validation Message'),Yii::t('dialog','Data is filled in incorrectly, please check and modify before proceeding')." error:not find new");
                     Yii::app()->getRequest()->redirect(Yii::app()->createUrl('commission/end',array('year'=>$years,'month'=>$months,'index'=>$index)));
                 }
                 $sql1="select * from acc_service_comm_hdr where year_no='".$year."' and month_no='".$month."' and city='".$records['city']."' and  concat_ws(' ',employee_name,employee_code)= '".$records['salesman']."' ";
@@ -1272,7 +1272,7 @@ class ReportXS01SList extends CListPageModel
                     }
                     if($records['all_number']!=NULL){
                         if($records['all_number']==0){
-                            Dialog::message(Yii::t('dialog','Validation Message'),Yii::t('dialog','Data is filled in incorrectly, please check and modify before proceeding')." service_id:".$records["id"] );
+                            Dialog::message(Yii::t('dialog','Validation Message'),Yii::t('dialog','Data is filled in incorrectly, please check and modify before proceeding')." error:all_number is 0");
                             Yii::app()->getRequest()->redirect(Yii::app()->createUrl('commission/end',array('year'=>$years,'month'=>$months,'index'=>$index)));
                         }
                         $new=$a/$records['all_number'];
@@ -1288,7 +1288,7 @@ class ReportXS01SList extends CListPageModel
                     }
 
                     if(!isset($m)){
-                        Dialog::message(Yii::t('dialog','Validation Message'),Yii::t('dialog','Data is filled in incorrectly, please check and modify before proceeding') );
+                        Dialog::message(Yii::t('dialog','Validation Message'),Yii::t('dialog','Data is filled in incorrectly, please check and modify before proceeding')." error:m" );
                         Yii::app()->getRequest()->redirect(Yii::app()->createUrl('commission/end',array('year'=>$years,'month'=>$months,'index'=>$index)));
                     }
                     $spanning=$this->getRoyalty($index,$city,$year,$month,$records['othersalesman']);
@@ -1310,7 +1310,7 @@ class ReportXS01SList extends CListPageModel
                     if($b>0){
                         if($records[$all_number]!=NULL){
                             if($records['all_number']==0){
-                                Dialog::message(Yii::t('dialog','Validation Message'),Yii::t('dialog','Data is filled in incorrectly, please check and modify before proceeding') );
+                                Dialog::message(Yii::t('dialog','Validation Message'),Yii::t('dialog','Data is filled in incorrectly, please check and modify before proceeding')." error:all_number is 0" );
                                 Yii::app()->getRequest()->redirect(Yii::app()->createUrl('commission/end',array('year'=>$years,'month'=>$months,'index'=>$index)));
                             }
                             $news=$b/$records[$all_number];
