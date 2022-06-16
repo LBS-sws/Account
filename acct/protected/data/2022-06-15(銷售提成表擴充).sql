2181c447cd9cee21022171eec8c1f165635abf6b
shenchao <shenchao@lbsgroup.com.cn>
2022-06-16 10:17:51 +0800

    銷售提成表擴充

--- acct/protected/data/2022-06-15(銷售提成表擴充).sql
+++ acct/protected/data/2022-06-15(銷售提成表擴充).sql
@@ -4,3 +4,5 @@
 -- ----------------------------
 ALTER TABLE acc_service_comm_dtl ADD COLUMN service_reward decimal(5,3) NOT NULL DEFAULT 0.000 COMMENT '服务奖励点' AFTER new_calc;
 ALTER TABLE acc_service_comm_dtl ADD COLUMN point decimal(5,3) NOT NULL DEFAULT 0.000 COMMENT '销售提成激励点' AFTER new_calc;
+ALTER TABLE acc_service_comm_dtl ADD COLUMN install_amount decimal(11,2) NOT NULL DEFAULT 0.00 COMMENT '裝機提成' AFTER performance_amount;
+ALTER TABLE acc_service_comm_dtl ADD COLUMN install_money decimal(11,2) NOT NULL DEFAULT 0.00 COMMENT '裝機營業額' AFTER performance_amount;
