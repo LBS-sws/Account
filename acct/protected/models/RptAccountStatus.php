6b20e051eaa2503a472ad4efa0b41e97ee685cee
shenchao <shenchao@lbsgroup.com.cn>
2024-07-24 16:00:05 +0800

    修改“每日收款择要”的计算逻辑

--- acct/protected/models/RptAccountStatus.php
+++ acct/protected/models/RptAccountStatus.php
@@ -41,9 +41,13 @@ class RptAccountStatus extends CReport {
 				order by a.acct_type_id, a.acct_name
 			";
 		$this->result1 = Yii::app()->db->createCommand($sql)->queryAll();
-
+		/*
 		$sql = "select swoper$suffix.IncomeYTD('00002','$city','$start_dt') as income_ytd,
 					swoper$suffix.IncomeMTD('00002','$city',('$month_start_dt' - interval 1 Minute)) as income_mtd
+			";
+		*/
+		$sql = "select operation$suffix.IncomeYTD('10011','$city','$start_dt') as income_ytd,
+					operation$suffix.IncomeMTD('10011','$city',('$month_start_dt' - interval 1 Minute)) as income_mtd
 			";
 		$this->result1_1 = Yii::app()->db->createCommand($sql)->queryRow();
 		
